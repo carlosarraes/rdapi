0262925814ac22269e6e5332480214f8877d076d
carraes <carraeshb@gmail.com>
2023-07-08 17:46:59 -0300

    feat: create user layer

---
+++ test/support/fixtures/users_fixtures.ex
@@ -0,0 +1,22 @@
+defmodule Rdapi.UsersFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `Rdapi.Users` context.
+  """
+
+  @doc """
+  Generate a user.
+  """
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(%{
+        full_name: "some full_name",
+        gender: "some gender",
+        biography: "some biography"
+      })
+      |> Rdapi.Users.create_user()
+
+    user
+  end
+end
